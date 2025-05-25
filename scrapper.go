80b7df803b35b4a9eee7f05641f206fc7ea7a0e0
unknown <anjoglekar0204@gmail.com>
2025-05-25 13:07:27 +0530

    Only Access Target HTML Tags in scrapper.go

--- scrapper.go
+++ scrapper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -10,11 +11,12 @@ import (
 func scrape(c *colly.Collector, url string) string {
 	var content string
 
-	c.OnHTML("*", func(e *colly.HTMLElement) {
-		fmt.Println("Tag:", e.Name, "Text:", e.Text)
-		content += e.Text
+	c.OnHTML("h1, h2, h3, h4, h5, h6, p, li", func(e *colly.HTMLElement) {
+		text := strings.TrimSpace(e.Text)
+		if text != "" {
+			content += text + "\n"
+		}
 	})
-
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
 	})
